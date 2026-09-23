497cd82027ea3288a7b62e56e4b2440e6c959d90
BrianLook <smithbrian@yahoo.com>
2026-09-23 18:38:33 -0400

    accessibility page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -35,6 +35,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.6,
     },
     {
+      url: `${baseUrl}/accessibility`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+    },
+    {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: "yearly",
