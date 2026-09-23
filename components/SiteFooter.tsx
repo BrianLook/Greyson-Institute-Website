25ce2b60dddda202a89b75af1fabb6ca5e37674a
BrianLook <smithbrian@yahoo.com>
2026-09-23 19:18:38 -0400

    Analytics Settings to footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import { BrandLockup } from "./BrandMark";
 
+const OPEN_SETTINGS_EVENT = "greyson-open-analytics-settings";
+
 export function SiteFooter() {
+  function openAnalyticsSettings() {
+    window.dispatchEvent(new Event(OPEN_SETTINGS_EVENT));
+  }
+
   return (
     <footer className="site-footer">
       <div className="container footer-grid">
@@ -39,6 +47,28 @@ export function SiteFooter() {
           <Link href="/privacy">Privacy</Link>
           <Link href="/terms">Terms</Link>
           <Link href="/accessibility">Accessibility</Link>
+
+          <button
+            type="button"
+            onClick={openAnalyticsSettings}
+            style={{
+              appearance: "none",
+              width: "fit-content",
+              minHeight: "24px",
+              padding: 0,
+              margin: 0,
+              border: 0,
+              background: "transparent",
+              color: "inherit",
+              font: "inherit",
+              cursor: "pointer",
+              display: "inline-flex",
+              alignItems: "center",
+              textAlign: "left",
+            }}
+          >
+            Analytics Settings
+          </button>
 
           <span>
             Greyson Institute is operated by BrightPath Education Group, LLC
