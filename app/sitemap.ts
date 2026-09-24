709bb697f551c42d7fe80fd8f9ef1a4c87a45451
BrianLook <smithbrian@yahoo.com>
2026-09-24 10:15:31 -0400

    63-hour pre-licensing guide to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -35,6 +35,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/florida-63-hour-real-estate-pre-licensing-course`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
