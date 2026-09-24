be24df9e607a4e5b77e9096e8f294f93bee639f1
BrianLook <smithbrian@yahoo.com>
2026-09-24 10:47:24 -0400

    after passing exam guide to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -47,6 +47,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/what-happens-after-you-pass-the-florida-real-estate-exam`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
